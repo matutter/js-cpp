44bde3a576a5d5c9f582788d395617cdd1d1f3f7
matutter <matutter4@gmail.com>
2017-04-19 00:57:34 +0000

    lang/parse: adding initial skeleten sourcecode for parser

--- src/lang/parse/parser.cpp
+++ src/lang/parse/parser.cpp
@@ -1,17 +1,37 @@
+#include "lang/parse/parser.h"
+#include "debug.h"
+#include "types.h"
 
-class CppJsParser {
-  
-  public:
-    CppJsParser();
-    virtual ~CppJsParser();
-  
-    //std::map<std::string, int> variables;
+#include <string>
 
-    void scan_begin ();
-    void scan_end ();
-    bool trace_scanning;
+namespace cppjs {
   
-    int parse(const std::string& input);
-    int parseFile(const std::string& path);
+  Parser::Parser() {
+    debug_info("New Parser");
+  };
+
+  VOID Parser::scan_begin() {
+    debug_info("Parser::scan_begin");
+  }
+  
+  VOID Parser::scan_end() {
+    debug_info("Parser::scan_end");
+  }
+  
+  int Parser::parse(const std::string& input) {
+    debug_info("Parser::parse");
+    return 0;
+  }
+  
+  int Parser::parseFile(const std::string& path) {
+    debug_info("Parser::parseFile");
+    return 0;
+  }
+
+  VOID Parser::error(const std::string& msg) {
+    debug_danger("Parser::error")
+  }
 
 };
+
+
